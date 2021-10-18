78476912318467c366300d9729d31d4592c218c8
Taniguchi1204 <tatsuya19951204@gmail.com>
2021-10-18 15:56:33 +0900

    リテラル

---
+++ array.js
@@ -0,0 +1,8 @@
+// 配列リテラル
+var data = ['javascript','Ajax','ASP.NET'];
+console.log(data[0]); //dataの１番目を取得
+
+var data = ['javascript',['jQuery', 'prototype.js'],'ASP.NET'];
+console.log(data[1[0]]); //dataの２番目要素である配列の１番目を取得（jQuery）
+
+
